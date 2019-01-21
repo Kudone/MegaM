7934cf406925103925988f7acc3f2d5ddea9cf2f
kudone <ku.kn8821@gmail.com>
2019-01-22 00:36:57 +0900

    no message

---
+++ Assets/_Scripts/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int speed = 10;
+    
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = transform.forward.normalized * speed;
+        Destroy(this.gameObject, 0.57f);
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag == "enemy")
+        {
+                Destroy(this.gameObject);
+        }
+    }
+}
