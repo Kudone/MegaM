8d06107329a028f1dd87876dc9bb6b37c7d788fe
kudone <ku.kn8821@gmail.com>
2019-01-22 19:52:50 +0900

    enemy action

--- Assets/_Scripts/EnemyShot.cs
+++ Assets/_Scripts/EnemyShot.cs
@@ -4,26 +4,35 @@ using UnityEngine;
 
 public class EnemyShot : MonoBehaviour
 {
+
+    public GameObject jakubllet;
+    private Animator animator;
+   
+    
     // Start is called before the first frame update
     void Start()
     {
+        //this.animator = GetComponent<Animator>();
         StartCoroutine("JakuShot");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    public GameObject jakubllet;
 
     IEnumerator JakuShot()
     {
         while (true)
         {
+            
             yield return new WaitForSeconds(1.2f);
-                Instantiate(jakubllet, transform.position, transform.rotation);
+            GetComponent<Animator>().SetTrigger("isJaku");
+            Instantiate(jakubllet, transform.position, transform.rotation);
+            
         }
     }
+   
 
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
 }
