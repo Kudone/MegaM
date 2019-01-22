bf7b5b96aa6f9ee7f9a7094b636b7ae1037534da
kudone <ku.kn8821@gmail.com>
2019-01-23 01:09:28 +0900

    kyo ok

---
+++ Assets/_Scripts/SpisCusoru.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpisCusoru : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this.gameObject, 0.2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
