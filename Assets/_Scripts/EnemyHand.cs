5a9608de833474e23a9de1a44ba9324d4abdaf27
kudone <ku.kn8821@gmail.com>
2019-01-23 16:13:45 +0900

    hand

---
+++ Assets/_Scripts/EnemyHand.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHand : MonoBehaviour
+{
+    public int speed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 pos = transform.position;
+        pos.y = 1;
+        transform.position = pos;
+        GetComponent<Rigidbody>().velocity = new Vector3(100f, 1.0f, 10f).normalized * speed;
+        Destroy(this.gameObject, 1.2f);
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/_Scripts/EnemyShot.cs
+++ Assets/_Scripts/EnemyShot.cs
@@ -8,6 +8,8 @@ public class EnemyShot : MonoBehaviour
     private AudioSource jakusource;
     public AudioClip kyouSound;
     private AudioSource kyousource;
+    public AudioClip handSound;
+    private AudioSource handsource;
 
     public GameObject jakubllet;
     private Animator animator;
@@ -18,9 +20,11 @@ public class EnemyShot : MonoBehaviour
     {
         jakusource = GetComponent<AudioSource>();
         kyousource = GetComponent<AudioSource>();
+        handsource = GetComponent<AudioSource>();
         //this.animator = GetComponent<Animator>();
         StartCoroutine("JakuShot");
         StartCoroutine("FinalShot");
+        StartCoroutine("GodHand");
     }
 
 
@@ -48,7 +52,7 @@ public class EnemyShot : MonoBehaviour
         while (true)
         {
 
-            yield return new WaitForSeconds(10f);
+            yield return new WaitForSeconds(15f);
             GetComponent<Animator>().SetTrigger("isFinal");
             kyousource.PlayOneShot(kyouSound, 1);
             StartCoroutine("Spia");
@@ -66,7 +70,22 @@ public class EnemyShot : MonoBehaviour
             Instantiate(spia, new Vector3(rx, 1.0f, rz), Quaternion.Euler(90, 0, 0));
         }
     }
-    
+
+    public GameObject hand;
+
+    IEnumerator GodHand()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(3f);
+            GetComponent<Animator>().SetTrigger("isHand");
+            handsource.PlayOneShot(handSound, 1);
+            float rz = UnityEngine.Random.Range(0, 3) * 2;
+            Instantiate(hand, new Vector3(-1.0f, 1.0f, rz), Quaternion.Euler(50, 0, 180));
+        }
+    }
+
+
 
     // Update is called once per frame
     void Update()
