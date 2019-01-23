a15cd72b31af5dc299514d787b93fb32e7a7bb06
kudone <ku.kn8821@gmail.com>
2019-01-23 22:24:57 +0900

    tubojaku

---
+++ Assets/_TsuBoss/_Tscripts/TsuJaku.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TsuJaku : MonoBehaviour
+{
+    public int speed = 10;
+
+    void Start()
+    {
+        Vector3 pos = transform.position;
+        pos.y = 1;
+        transform.position = pos;
+        GetComponent<Rigidbody>().velocity = transform.forward.normalized * speed;
+        Destroy(this.gameObject, 0.57f);
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
+
