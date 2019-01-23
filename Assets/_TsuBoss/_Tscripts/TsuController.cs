ebc3cdc9337a8c58d930288dcc076f8253b78051
kudone <ku.kn8821@gmail.com>
2019-01-23 19:53:52 +0900

    tsubo

---
+++ Assets/_TsuBoss/_Tscripts/TsuController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TsuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Action");
+    }
+
+    int vmax = 4;
+    int hmax = 10;
+    int hmin = 6;
+
+    int[] v = { -2, 0, 2 };
+    int[] h = { -2, 0, 2 };
+
+    IEnumerator Action()
+    {
+
+        while (true)
+        {
+            yield return new WaitForSeconds(0.5f);
+            int vr = UnityEngine.Random.Range(0, 3);
+            int hr = UnityEngine.Random.Range(0, 3);
+
+            Transform enemyTrans = GameObject.Find("Tuboss").transform;
+            Vector3 pos = enemyTrans.position;
+
+
+            float result_x = pos.x + v[vr];
+            float result_z = pos.z + h[hr];
+
+            if (result_x < 0 || result_x > vmax)
+            {
+                result_x = pos.x;
+            }
+
+            if (result_z < hmin || result_z > hmax)
+            {
+                result_z = pos.z;
+            }
+
+            pos.x = result_x;
+            pos.z = result_z;
+
+            enemyTrans.position = pos;
+
+        }
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
